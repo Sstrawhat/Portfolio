795e0bfd89771218a7b80963ede2d9ef338dbf4f
Ashley Fajardo <39252599+Sstrawhat@users.noreply.github.com>
2024-10-26 15:41:00 +0800

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,56 @@
+
+var homesection = document.getElementById('HOME');
+
+var aboutsection = document.getElementById('ABOUT');
+
+var skillsection = document.getElementById('SKILL');
+
+var projectsection = document.getElementById('PROJECT');
+
+var contactsection = document.getElementById('CONTACT');
+
+const options = {
+    rootMargin:"120px",
+    threshold : 0.5
+}
+
+const observer = new IntersectionObserver(callBack, options);
+
+
+
+function callBack(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            switch (entry['target']['id']) {
+                case "HOME":
+                    document.getElementById('img-home-section').classList.add('show')
+                    document.getElementById('home-content').classList.add('show')
+                    break;
+                case "ABOUT":
+                    document.getElementById('img-about-section').classList.add('show')
+                    document.getElementById('about-content').classList.add('show')
+                    break;
+                case "SKILL":
+                    document.getElementById('img-skill-section').classList.add('show')
+                    document.getElementById('img-skill-2-section').classList.add('show')
+                    break;
+                case "PROJECT":
+                    document.getElementById('img-project-section').classList.add('show')
+                    break;
+                case "CONTACT":
+                    document.getElementById('img-contact-section').classList.add('show')
+                    break;
+            }
+        }
+    });
+}
+
+//   entries.forEach(element => {
+//   
+//   });
+
+observer.observe(homesection);
+observer.observe(aboutsection);
+observer.observe(skillsection);
+observer.observe(projectsection);
+observer.observe(contactsection);
