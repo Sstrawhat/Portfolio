bdfc926fc78d3a50d8a0f46bb064b10134a3db62
Ashley Fajardo <39252599+Sstrawhat@users.noreply.github.com>
2025-04-22 07:56:03 +0800

    First Commit

---
+++ src/Component/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import Content from "./Content"
+import Navigation from "./Navigation"
+
+
+
+const Layout = () => (
+  <div className="layout">
+    <Content />
+    <Navigation />
+  </div>
+)
+
+export default Layout
---
+++ src/Component/Layout/Navigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='navigation'>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Skill</a></li>
+            <li><a href='#'>Experience</a></li>
+            <li><a href='#'>Project</a></li>
+            <li><a href='#'>Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navigation
